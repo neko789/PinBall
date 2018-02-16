db90f9110cf572f87b1c7b137492a1457020529f
takeru morimoto <nice.hermori@gmail.com>
2018-02-16 23:16:07 +0900

    :

---
+++ Assets/BallController.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class BallController : MonoBehaviour
+{
+    private int score = 0;
+    private int totalscore = 0;
+
+    //ボールが見える可能性のあるz軸の最大値
+    private float visiblePosZ = -6.5f;
+
+    //ゲームオーバを表示するテキスト
+    private GameObject gameoverText;
+    private GameObject TotalScoreText;
+
+    // Use this for initialization
+    void Start()
+    {
+        //シーン中のGameOverTextオブジェクトを取得
+        this.gameoverText = GameObject.Find("GameOverText");
+        this.TotalScoreText = GameObject.Find("TotalScoreText");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.TotalScoreText.GetComponent<Text>().text = "TotalScore :" + totalscore;
+        //ボールが画面外に出た場合
+        if (this.score >= 0)
+        {
+            this.totalscore += this.score;
+            this.score -= this.score;
+        }
+        if (this.transform.position.z < this.visiblePosZ)
+        {
+            //GameoverTextにゲームオーバを表示
+            this.gameoverText.GetComponent<Text>().text = "Game Over";
+        }
+    }
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "SmallStarTag")
+        {
+            this.score = 10;
+        }
+        else if (other.gameObject.tag == "LargeStarTag")
+        {
+            this.score = 50 ;
+        }
+        else if (other.gameObject.tag == "SmallCloudTag" || tag == "LargeCloudTag")
+        {
+            this.score = 20;
+        }
+
+    }
+}
---
+++ Assets/FripperController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FripperController : MonoBehaviour {
+    private HingeJoint myhingeJoint;
+
+    private float defaultAngle = 20;
+
+    private float flickAngle = -20;
+
+	// Use this for initialization
+	void Start () {
+        this.myhingeJoint = GetComponent<HingeJoint>();
+
+        setAngle(this.defaultAngle);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if(Input.GetKeyDown(KeyCode.LeftArrow)&& tag == "LeftFripperTag")
+        {
+            setAngle(this.flickAngle);
+        }
+        if(Input.GetKeyDown(KeyCode.RightArrow)&& tag == "RightFripperTag")
+        {
+            setAngle(this.flickAngle);
+        }
+        if(Input.GetKeyUp(KeyCode.LeftArrow)&& tag == "LeftFripperTag")
+        {
+            setAngle(this.defaultAngle);
+        }
+        if(Input.GetKeyUp(KeyCode.RightArrow)&& tag == "RightFripperTag")
+        {
+            setAngle(this.defaultAngle);
+        }
+	} 
+    public void setAngle (float angle)
+    {
+        JointSpring jointSpr = this.myhingeJoint.spring;
+        jointSpr.targetPosition = angle;
+        this.myhingeJoint.spring = jointSpr;
+
+    }
+}
